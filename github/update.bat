bb504f455de959564705798fdce5247581236bc8
BlackLabelAppsSoftware <apps.black.label@gmail.com>
2026-01-03 03:41:38 +0100

    update sáb. 03/01/2026  3:41:38.09

---
+++ github/update.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ===============================
+echo 🔄 Actualizando GitHub Repo
+echo ===============================
+
+cd /d F:\development\flutter\colegiosoftware
+
+git status
+git add .
+git commit -m "update %date% %time%"
+git push
+
+echo ===============================
+echo ✅ Repo actualizado correctamente
+echo ===============================
+pause
